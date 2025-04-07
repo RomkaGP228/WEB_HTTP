00fee229fc3c46b435e8f94d39a49146021b2efb
geo_prepod_pc <romkagp@yandex.ru>
2025-04-07 19:08:22 +0300

    Какой район?

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
-import sys
-from io import BytesIO  # Этот класс поможет нам сделать картинку из потока байт
 import requests
-from PIL import Image
-from get_spn import get_spn
+from get_coords import get_coords
+import sys
 
 
 def main():
-    toponym_to_find = " ".join(sys.argv[1:])
-
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-
-    geocoder_params = {
-        "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
-        "geocode": toponym_to_find,
-        "format": "json"}
-
-    response = requests.get(geocoder_api_server, params=geocoder_params)
-
-    if not response:
-        print("Произошла ошибка! Выход из программы...", response)
-        exit(0)
-    json_response = response.json()
-    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-    toponym_coordinates = toponym["Point"]["pos"]
-    toponym_longitude, toponym_latitude = toponym_coordinates.split(" ")
-    delta_longitude, delta_latitude = get_spn(toponym)
-    apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
-    map_params = {
-        "ll": ",".join([toponym_longitude, toponym_latitude]),
-        "spn": ",".join([delta_longitude, delta_latitude]),
-        "apikey": apikey,
-        "pt": ",".join([toponym_longitude, toponym_latitude, "comma"])
-    }
-    map_api_server = "https://static-maps.yandex.ru/v1"
-    response = requests.get(map_api_server, params=map_params)
-    im = BytesIO(response.content)
-    opened_image = Image.open(im)
-    opened_image.show()
+    coords = get_coords(' '.join(sys.argv[1:]))
+    geocoder_server = "http://geocode-maps.yandex.ru/1.x/"
+    params = {"geocode": ','.join(list(map(str, coords))),
+              "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+              "kind": "district",
+              "format": "json",
+              "results": 1}
+    data = requests.get(geocoder_server, params).json()
+    print(data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['name'])
 
 
 if __name__ == '__main__':
